9028774cbb81ff4e9e4a1d8c65eecd1af7556b1c
wunglee <wunglee@qq.com>
2021-05-19 13:09:20 +0800

    重构,将转账命令处理器改为单例

--- src/转账BC/静态初始化.cpp
+++ src/转账BC/静态初始化.cpp
@@ -4,6 +4,7 @@
 #include "从适配器/转账仓储.h"
 #include "从适配器/转账DAO.h"
 #include "主适配器/Protobuf请求适配器.h"
+#include "主适配器/转账命令处理器.h"
 
 转账服务 * 转账服务::转账服务_= nullptr;
 转账查询 * 转账查询::转账查询_= nullptr;
@@ -11,3 +12,4 @@
 转账仓储 * 转账仓储::转账仓储_= nullptr;
 转账DAO * 转账DAO::转账DAO_= nullptr;
 Protobuf请求适配器 * Protobuf请求适配器::PB请求适配器_= nullptr;
+转账命令处理器* 转账命令处理器::转账命令处理器_= nullptr;
